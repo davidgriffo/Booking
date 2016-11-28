0ae8ce112c971e4a63ada5d60b2a00fec568e161
Grumme <jakobgrumsen@gmail.com>
2016-11-28 12:20:08 +0100

    Can select bookings now, and fixed the view

--- Booking/Controllers/SelectedBookingController.cs
+++ Booking/Controllers/SelectedBookingController.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Booking.Authorization;
-using Booking.Models;
 using Dll;
 using Dll.Entities;
 using Dll.Gateways;
 
 namespace Booking.Controllers
 {
-    [RequireUser]
-    public class HomeController : Controller
+    public class SelectedBookingController : Controller
     {
         private IGateway<Room, int> _rg = new DllFacade().GetRoomGateway();
-        private IGateway<Equipment, int> _eg = new DllFacade().GetEquipmentGateway();
-
-        public ActionResult Index()
+        // GET: SelectedBooking
+        public ActionResult Index(int id)
         {
-            return View(_rg.Read());
+            ;
+            return View(_rg.Read(id));
+        }
+
+        public ActionResult BookRoom()
+        {
+
+
+            return View();
         }
     }
 }
