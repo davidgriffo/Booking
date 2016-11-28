1a2fa8fb084308eefd43bb897d61059fe3189305
Grumme <jakobgrumsen@gmail.com>
2016-11-28 11:30:27 +0100

    Create locals is done

--- Dll/Entities/Room.cs
+++ Dll/Entities/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace Dll.Entities
     public class Room
     {
         public int Id { get; set; }
+        [DisplayName("Navn")]
         public String Name { get; set; }
+        [DisplayName("Afdeling")]
         public string Department { get; set; }
+        [DisplayName("Beskrivelse")]
         public String Description { get; set; }
+        [DisplayName("Antal personer")]
         public int Capacity { get; set; }
+        [DisplayName("Udstyr")]
         public List<Equipment> Equipment { get; set; }
     }
 }
