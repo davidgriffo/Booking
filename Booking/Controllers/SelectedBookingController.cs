3b7da0dc542b6f3d887ee056123bad8fd0948dab
Rasmusmm96 <rallemm96@gmail.com>
2016-11-30 11:34:28 +0100

    Now able to book a room

--- Booking/Controllers/SelectedBookingController.cs
+++ Booking/Controllers/SelectedBookingController.cs
@@ -6,23 +6,38 @@ using System.Web.Mvc;
 using Dll;
 using Dll.Entities;
 using Dll.Gateways;
+using Booking = Dll.Entities.Booking;
 
 namespace Booking.Controllers
 {
     public class SelectedBookingController : Controller
     {
         private IGateway<Room, int> _rg = new DllFacade().GetRoomGateway();
+        private IGateway<Dll.Entities.Booking, int> _bg = new DllFacade().GetBookingGateway();
+        private IAccountGateway _ag = new DllFacade().GetAccountGateway();
+
         // GET: SelectedBooking
         public ActionResult Index(int id)
         {
-            ;
             return View(_rg.Read(id));
         }
 
-        public ActionResult BookRoom()
+        public ActionResult BookRoom(string startDate, string endDate, int roomId)
         {
+            var startDateConverted = Convert.ToDateTime(startDate);
+            var endDateConverted = Convert.ToDateTime(endDate);
 
-
+            var user = _ag.GetUserLoggedIn();
+            var booking = new Dll.Entities.Booking()
+            {
+                Creator = user,
+                Room = _rg.Read(roomId),
+                FromDate = startDateConverted,
+                ToDate = endDateConverted,
+                Accepted = new List<User>(),
+                Invited = new List<User>()
+            };
+            _bg.Create(booking);
             return View();
         }
     }
---
+++ Booking/Models/BookingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Booking.Controllers;
+using Dll.Entities;
+
+namespace Booking.Models {
+    public class BookingViewModel {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Room Room { get; set; }
+    }
+}
