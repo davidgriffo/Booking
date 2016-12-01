f724e9d58ea2c26a0527a1e70559b8a9f228af76
Peeaks <Kristoffer2509@icloud.com>
2016-12-01 09:14:33 +0100

    Fixed something

--- Booking/Controllers/SelectedBookingController.cs
+++ Booking/Controllers/SelectedBookingController.cs
@@ -1,35 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Dll;
 using Dll.Entities;
 using Dll.Gateways;
-using Booking = Dll.Entities.Booking;
 
-namespace Booking.Controllers
-{
-    public class SelectedBookingController : Controller
-    {
+namespace Booking.Controllers {
+    public class SelectedBookingController : Controller {
         private IGateway<Room, int> _rg = new DllFacade().GetRoomGateway();
         private IGateway<Dll.Entities.Booking, int> _bg = new DllFacade().GetBookingGateway();
         private IAccountGateway _ag = new DllFacade().GetAccountGateway();
 
         // GET: SelectedBooking
-        public ActionResult Index(int id)
-        {
+        public ActionResult Index(int id) {
             return View(_rg.Read(id));
         }
 
-        public ActionResult BookRoom(string startDate, string endDate, int roomId)
-        {
-            var startDateConverted = Convert.ToDateTime(startDate);
-            var endDateConverted = Convert.ToDateTime(endDate);
+        public ActionResult BookRoom(string startDate, string endDate, int roomId) {
+            DateTimeFormatInfo dk = new CultureInfo("da-DK", false).DateTimeFormat;
+            var startDateConverted = Convert.ToDateTime(startDate, dk);
+            var endDateConverted = Convert.ToDateTime(endDate, dk);
 
             var user = _ag.GetUserLoggedIn();
-            var booking = new Dll.Entities.Booking()
-            {
+            var booking = new Dll.Entities.Booking() {
                 Creator = user,
                 Room = _rg.Read(roomId),
                 FromDate = startDateConverted,
@@ -60,13 +56,11 @@ namespace Booking.Controllers
             }
             if (isAvailible) {
                 createdBooking = _bg.Create(booking);
-            }
-            else
-            {
+            } else {
                 createdBooking = null;
             }
 
-            
+
             return View(createdBooking);
         }
     }
